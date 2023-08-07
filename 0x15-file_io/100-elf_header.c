e75c6c5337390556557f1452482014bd1bb87a95
Abdelkarim Douadjia <abdelkarim.douadjia@gmail.com>
2023-08-07 18:38:41 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,11 +11,6 @@ void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
-/*void print_abi(unsigned char *e_ident);*/
-/*void print_osabi(unsigned char *e_ident);*/
-/*void print_type(unsigned int e_type, unsigned char *e_ident);*/
-//void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-//void close_elf(int elf);
 
 /**
  * check_elf - Checks if a file is an ELF file.
