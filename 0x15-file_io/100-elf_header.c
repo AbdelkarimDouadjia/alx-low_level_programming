2e30c2e652e68df801517f53718eb667af1f4194
Abdelkarim Douadjia <abdelkarim.douadjia@gmail.com>
2023-08-07 18:06:29 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -112,9 +112,9 @@ void print_data(unsigned char *e_ident)
 }
 
 /**
- *  * print_version - Prints the version of an ELF header.
- *   * @e_ident: A pointer to an array containing the ELF version.
- *    */
+ ** print_version - Prints the version of an ELF header.
+ ** @e_ident: A pointer to an array containing the ELF version.
+ **/
 void print_version(unsigned char *e_ident)
 {
 	 printf(" Version: %d",
